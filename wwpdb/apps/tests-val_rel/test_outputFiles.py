3869abc3a04e0dc9b38503e9e2276957ae36a08d
John Berrisford <berrisfordjohn@gmail.com>
2019-10-29 21:13:47 +0000

    added more tests

--- wwpdb/apps/tests-val_rel/test_outputFiles.py
+++ wwpdb/apps/tests-val_rel/test_outputFiles.py
@@ -7,34 +7,66 @@ from wwpdb.apps.val_rel.outputFiles import outputFiles
 class OutputFilesTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.pdbid = '1cbs'
+        self.pdbid_hash = self.pdbid[1:3]
+        self.emdbid = 'EMD-1234'
+        self.emdb_accession = 'emd_1234'
+        self.output_folder = os.path.join(os.sep, 'nfs', 'test')
+
+    def test_get_dir(self):
+        final_output_folder = ''
+        of = outputFiles(outputRoot=self.output_folder)
+        ret = of.get_entry_output_folder()
+        self.assertTrue(ret == final_output_folder)
 
     def test_get_pdbid_dir(self):
-        pdbid = '1cbs'
-        pdb_hash = pdbid[1:3]
-        output_folder = os.path.join(os.sep, 'nfs', 'test')
-        final_output_folder = os.path.join(output_folder, pdb_hash, pdbid)
-        of = outputFiles(pdbID=pdbid, outputRoot=output_folder)
+        final_output_folder = os.path.join(self.output_folder, self.pdbid_hash, self.pdbid)
+        of = outputFiles(pdbID=self.pdbid, outputRoot=self.output_folder)
         ret = of.get_entry_output_folder()
         self.assertTrue(ret == final_output_folder)
 
     def test_get_emdb_dir(self):
-        emdbid = 'EMD-1234'
-        output_folder = os.path.join(os.sep, 'nfs', 'test')
-        final_output_folder = os.path.join(output_folder, 'emd', emdbid, 'validation')
-        of = outputFiles(emdbID=emdbid, outputRoot=output_folder)
+        final_output_folder = os.path.join(self.output_folder, 'emd', self.emdbid, 'validation')
+        of = outputFiles(emdbID=self.emdbid, outputRoot=self.output_folder)
         ret = of.get_entry_output_folder()
         self.assertTrue(ret == final_output_folder)
 
     def test_get_pdbid_and_emdb_dir(self):
-        pdbid = '1cbs'
-        emdbid = 'EMD-1234'
-        pdb_hash = pdbid[1:3]
-        output_folder = os.path.join(os.sep, 'nfs', 'test')
-        final_output_folder = os.path.join(output_folder, pdb_hash, pdbid)
-        of = outputFiles(pdbID=pdbid, outputRoot=output_folder)
+        final_output_folder = os.path.join(self.output_folder, self.pdbid_hash, self.pdbid)
+        of = outputFiles(pdbID=self.pdbid, outputRoot=self.output_folder)
         ret = of.get_entry_output_folder()
         self.assertTrue(ret == final_output_folder)
+
+    def test_get_pdbid_dir_skip_hash(self):
+        final_output_folder = os.path.join(self.output_folder, self.pdbid)
+        of = outputFiles(pdbID=self.pdbid, outputRoot=self.output_folder, skip_pdb_hash=True)
+        ret = of.get_entry_output_folder()
+        self.assertTrue(ret == final_output_folder)
+
+    def test_get_pdbid_dir_emdb_set_first(self):
+        final_output_folder = os.path.join(self.output_folder, self.pdbid_hash, self.pdbid)
+        of = outputFiles(emdbID=self.emdbid, outputRoot=self.output_folder)
+        of.set_pdb_id(entry_id=self.pdbid)
+        ret = of.get_entry_output_folder()
+        self.assertTrue(ret == final_output_folder)
+
+    def test_set_accession_pdbid(self):
+        of = outputFiles(pdbID=self.pdbid)
+        ret = of.set_accession()
+        self.assertTrue(ret == self.pdbid)
+
+    def test_set_accession_emdbid(self):
+        of = outputFiles(emdbID=self.emdbid)
+        ret = of.set_accession()
+        self.assertTrue(ret == self.emdb_accession)
+
+    def test_set_accession_pdbid_and_emdbid(self):
+        of = outputFiles(pdbID=self.pdbid, emdbID=self.emdbid)
+        of.set_accession_variables(with_emdb=True)
+        ret = of.set_accession()
+        accession = '{}_{}'.format(self.emdb_accession, self.pdbid)
+        self.assertTrue(ret == accession)
+
 
 if __name__ == '__main__':
     unittest.main()
--- wwpdb/apps/val_rel/outputFiles.py
+++ wwpdb/apps/val_rel/outputFiles.py
@@ -2,13 +2,15 @@ import logging
 import os
 
 class outputFiles:
-    def __init__(self, pdbID=None, emdbID=None, outputRoot=None):
+    def __init__(self, pdbID=None, emdbID=None, outputRoot='', skip_pdb_hash=False):
         self._pdbID = pdbID
         self._emdbID = emdbID
         self.output_root = outputRoot
         self._entryID = None
-        self.skip_pdb_hash = False
+        self.skip_pdb_hash = skip_pdb_hash
         self.entry_output_folder = self.get_entry_output_folder()
+        self.with_emdb = False
+        self.copy_to_root_emdb = False
 
     def set_entry_id(self, entry_id):
         self._entryID = entry_id
@@ -44,34 +46,43 @@ class outputFiles:
             return self._entryID
         return ''
 
-    def get_core_validation_files(self, with_emdb=False, copy_to_root_emdb=False):
+    def set_accession_variables(self, with_emdb=False, copy_to_root_emdb=False):
+        self.with_emdb = with_emdb
+        self.copy_to_root_emdb = copy_to_root_emdb
+
+    def set_accession(self):
+        self.accession = "{}".format(self.get_entry_id())
+        if self.get_emdb_id() and not self.get_pdb_id():
+            self.accession = self.get_emdb_lower_underscore()
+        if self.get_pdb_id() and self.get_emdb_id() and self.with_emdb:
+            self.accession = "{}_{}".format(self.get_emdb_lower_underscore(), self.get_pdb_id())
+        if self.get_emdb_id() and self.copy_to_root_emdb:
+            self.accession = "{}".format(self.get_emdb_lower_underscore())
+        
+        return self.accession
+
+    def get_core_validation_files(self):
         logging.debug("getting core files for: {}".format(self.get_entry_id()))
         logging.debug(
             "path: {}".format(os.path.join(self.get_entry_output_folder(), self.get_entry_id()))
         )
 
-        accession = "{}".format(self.get_entry_id())
-        if self.get_emdb_id() and not self.get_pdb_id():
-            accession = self.get_emdb_lower_underscore()
-        if self.get_pdb_id() and self.get_emdb_id() and with_emdb:
-            accession = "{}_{}".format(self.get_emdb_lower_underscore(), self.get_pdb_id())
-        if self.get_emdb_id() and copy_to_root_emdb:
-            accession = "{}".format(self.get_emdb_lower_underscore())
+        self.get_accession()
 
         validationReportPath = os.path.join(
-            self.get_entry_output_folder(), accession + "_validation.pdf"
+            self.get_entry_output_folder(), self.accession + "_validation.pdf"
         )
         xmlReportPath = os.path.join(
-            self.get_entry_output_folder(), accession + "_validation.xml"
+            self.get_entry_output_folder(), self.accession + "_validation.xml"
         )
         validationFullReportPath = os.path.join(
-            self.get_entry_output_folder(), accession + "_full_validation.pdf"
+            self.get_entry_output_folder(), self.accession + "_full_validation.pdf"
         )
         pngReportPath = os.path.join(
-            self.get_entry_output_folder(), accession + "_multipercentile_validation.png"
+            self.get_entry_output_folder(), self.accession + "_multipercentile_validation.png"
         )
         svgReportPath = os.path.join(
-            self.get_entry_output_folder(), accession + "_multipercentile_validation.svg"
+            self.get_entry_output_folder(), self.accession + "_multipercentile_validation.svg"
         )
 
         ret = {}
@@ -81,16 +92,7 @@ class outputFiles:
         ret["png"] = pngReportPath
         ret["svg"] = svgReportPath
 
-        return (
-            [
-                validationReportPath,
-                xmlReportPath,
-                validationFullReportPath,
-                pngReportPath,
-                svgReportPath,
-            ],
-            ret,
-        )
+        return ret
 
     def get_extra_validation_files(self):
         coef2foReportPath = os.path.join(
@@ -104,18 +106,16 @@ class outputFiles:
         ret["2fofc"] = coef2foReportPath
         ret["fofc"] = coeffoReportPath
 
-        return [coef2foReportPath, coeffoReportPath], ret
+        return ret
 
     def get_all_validation_files(self):
-        core_files, core_file_dict = self.get_core_validation_files()
-        extra_files, extra_file_dict = self.get_extra_validation_files()
-        all_files = core_files
-        all_files.extend(extra_files)
+        core_file_dict = self.get_core_validation_files()
+        extra_file_dict = self.get_extra_validation_files()
 
         all_file_dict = core_file_dict.copy()
         all_file_dict.update(extra_file_dict)
 
-        return all_files, all_file_dict
+        return all_file_dict
 
     def get_pdb_output_folder(self):
         self.set_entry_id(self.get_pdb_id())
