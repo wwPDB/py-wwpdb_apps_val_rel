88e1bea2f854a80f57da5b37c0269034d0ddd308
John Berrisford <berrisfordjohn@gmail.com>
2019-10-29 20:33:18 +0000

    starting tests

--- wwpdb/apps/tests-val_rel/test_outputFiles.py
+++ wwpdb/apps/tests-val_rel/test_outputFiles.py
@@ -1,20 +1,40 @@
 import unittest
 import logging
 import os
-from wwpdb.apps.val_rel import outputFiles
+from wwpdb.apps.val_rel.outputFiles import outputFiles
 
 
 class OutputFilesTests(unittest.TestCase):
 
-    def __init__(self):
+    def setUp(self):
         pass
 
     def test_get_pdbid_dir(self):
         pdbid = '1cbs'
-        output_folder = os.path.join('nfs', 'test')
-        of = outputFiles(pdbID=pdbid, output_folder=output_folder)
+        pdb_hash = pdbid[1:3]
+        output_folder = os.path.join(os.sep, 'nfs', 'test')
+        final_output_folder = os.path.join(output_folder, pdb_hash, pdbid)
+        of = outputFiles(pdbID=pdbid, outputRoot=output_folder)
         ret = of.get_entry_output_folder()
-        print(ret)
+        self.assertTrue(ret == final_output_folder)
+
+    def test_get_emdb_dir(self):
+        emdbid = 'EMD-1234'
+        output_folder = os.path.join(os.sep, 'nfs', 'test')
+        final_output_folder = os.path.join(output_folder, 'emd', emdbid, 'validation')
+        of = outputFiles(emdbID=emdbid, outputRoot=output_folder)
+        ret = of.get_entry_output_folder()
+        self.assertTrue(ret == final_output_folder)
+
+    def test_get_pdbid_and_emdb_dir(self):
+        pdbid = '1cbs'
+        emdbid = 'EMD-1234'
+        pdb_hash = pdbid[1:3]
+        output_folder = os.path.join(os.sep, 'nfs', 'test')
+        final_output_folder = os.path.join(output_folder, pdb_hash, pdbid)
+        of = outputFiles(pdbID=pdbid, outputRoot=output_folder)
+        ret = of.get_entry_output_folder()
+        self.assertTrue(ret == final_output_folder)
 
 if __name__ == '__main__':
     unittest.main()
