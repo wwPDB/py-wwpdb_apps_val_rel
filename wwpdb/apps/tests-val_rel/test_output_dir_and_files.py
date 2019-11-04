cc991986ac34e6f34818d374810cfc14fdef0c8d
John Berrisford <berrisfordjohn@gmail.com>
2019-11-04 21:42:34 +0000

    added tests for output dir

--- wwpdb/apps/tests-val_rel/test_output_dir_and_files.py
+++ wwpdb/apps/tests-val_rel/test_output_dir_and_files.py
@@ -13,15 +13,32 @@ class OuputFolderTests(unittest.TestCase):
 
     def setUp(self):
         self.pdbid = '1cbs'
+        self.pdbid_hash = self.pdbid[1:3]
         self.emdb = 'EMD-1234'
         self.output_folder = '/nfs/test'
         self.rv = runValidation()
 
     def test_pdbid(self):
+        output_dir = os.path.join(self.output_folder, self.pdbid_hash, self.pdbid)
         self.rv.pdbid = self.pdbid
         self.rv.outputRoot = self.output_folder
         self.rv.set_output_dir_and_files()
-        print(self.rv.entry_output_folder)
+        self.assertTrue(self.rv.entry_output_folder == output_dir)
+
+    def test_emdbid(self):
+        output_dir = os.path.join(self.output_folder, self.emdb)
+        self.rv.emdbid = self.emdb
+        self.rv.outputRoot = self.output_folder
+        self.rv.set_output_dir_and_files()
+        self.assertTrue(self.rv.entry_output_folder == output_dir)
+
+    def test_pdbid_and_emdbid(self):
+        output_dir = os.path.join(self.output_folder, self.pdbid_hash, self.pdbid)
+        self.rv.pdbid = self.pdbid
+        self.rv.emdbid = self.emdb
+        self.rv.outputRoot = self.output_folder
+        self.rv.set_output_dir_and_files()
+        self.assertTrue(self.rv.entry_output_folder == output_dir)
 
 if __name__ == '__main__':
     unittest.main()
