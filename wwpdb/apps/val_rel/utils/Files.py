dad6f1b2109f99ca381f82bce3030d17f6ad0605
jmb <jmb@ebi.ac.uk>
2020-06-01 08:41:17 +0100

    corrected copying of files to output folder

--- wwpdb/apps/val_rel/utils/Files.py
+++ wwpdb/apps/val_rel/utils/Files.py
@@ -17,7 +17,7 @@ def gzip_file(in_file, output_folder):
     """Compresses file in_file to in_file.gz.  Not be memory efficient as reads in file at one"""
     output_gzipped_file = get_gzip_name(in_file)
     if os.path.exists(in_file):
-        with open(in_file, 'r') as f_in, gzip.open(get_gzip_name(output_gzipped_file), "wb") as f_out:
+        with open(in_file, 'r') as f_in, gzip.open(output_gzipped_file, "wb") as f_out:
             f_out.writelines(f_in)
         if os.path.exists(output_gzipped_file):
             copy_file(in_file=output_gzipped_file, output_folder=output_folder)
