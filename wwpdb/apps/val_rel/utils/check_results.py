6e271b1755da88f73d64eb772a08c7028c8726ef
jmb <jmb@ebi.ac.uk>
2020-06-01 16:18:23 +0100

    improved checking script so it can be accessed programatically if needed

--- wwpdb/apps/val_rel/utils/check_results.py
+++ wwpdb/apps/val_rel/utils/check_results.py
@@ -1,16 +1,18 @@
-
-import os
 import argparse
 import logging
+import os
 from pprint import pprint
+
+from wwpdb.apps.validation.src.utils.validation_xml_reader import ValidationXMLReader
+
 from wwpdb.apps.val_rel.ValidateRelease import runValidation
 from wwpdb.apps.val_rel.utils.Files import get_gzip_name
 from wwpdb.apps.val_rel.utils.FindEntries import FindEntries
-from wwpdb.apps.validation.src.utils.validation_xml_reader import ValidationXMLReader
 
 logger = logging.getLogger(__name__)
 
-class checkResult:
+
+class CheckResult:
 
     def __init__(self, output_folder=None, pdbid=None, emdbid=None):
         self.__output_folder = output_folder
@@ -21,7 +23,7 @@ class checkResult:
         self.missing_files = {}
         self.validation_xml = None
         self.failed_programs = []
-        
+
     def __prepare_message(self):
         self.__message["pdbID"] = self.__pdbid
         self.__message["emdbID"] = self.__emdbid
@@ -43,7 +45,7 @@ class checkResult:
             gzipped_output_file = get_gzip_name(output_file)
             if not os.path.exists(gzipped_output_file):
                 self.missing_files.setdefault(output_file_type, []).append({self.rv.getEntryId(): gzipped_output_file})
-        
+
         self.check_failed_programs()
 
     def get_missing_files(self):
@@ -58,63 +60,72 @@ class checkResult:
     def get_failed_programs(self):
         return self.failed_programs
 
-def check_entries(entry_list, entry_type, output_folder=None):
-    ret = {}
-    
-    for entry in entry_list:
-        cr = None
-        if entry_type == 'pdb':
-            cr = checkResult(output_folder=output_folder, pdbid=entry)
-        elif entry_type == 'emdb':
-            cr = checkResult(output_folder=output_folder, emdbid=entry)
-        else:
-            logging.error('Unknown entry type')
-            return {}
-        cr.check_entry()
-        missing_ret = cr.get_missing_files()
-        for missing_type in missing_ret:
-            ret.setdefault(missing_type, []).append(missing_ret[missing_type])
-        ret_failed = cr.get_failed_programs()
-        if ret_failed:
-            for program in ret_failed:
-                ret.setdefault('failed_programs', {}).setdefault(program, []).append(entry)
 
-    return ret
-    
+class CheckEntries:
 
-def prepare_entries_and_check(output_folder=None, entry_file=None, entry_list=None, pdbids=True, emdbids=False, pdb_release=False, pdb_modified=False, emdb_release=False):
+    def __init__(self):
+        self.entry_list = []
+        self.return_dictionary = {}
+        self.failed_entries = []
+        self.entries_with_failed_programs = []
 
-    entries_to_check = {}
-    fe = FindEntries()
-    missing_files = {}
-    
-    if entry_file:
-        with open(entry_file) as entryFile:
-            for line in entryFile:
-                if line:
-                    entry = line.strip()
-                    if pdbids:
-                        entries_to_check.setdefault('pdb', []).append(entry)
-                    else:
-                        entries_to_check.setdefault('emdb', []).append(entry)
-    if entry_list:
-        if pdbids:
-            entries_to_check.setdefault('pdb', []).extend(entry_list)
-        else:
-            entries_to_check.setdefault('pdb', []).extend(entry_list)
+    def get_entries(self, pdb_added=False, pdb_modified=False, emdb_release=False):
+        fe = FindEntries()
+        pdb_entries = []
+        emdb_entries = []
+        if pdb_added:
+            pdb_entries.extend(fe.get_added_pdb_entries())
+        if pdb_modified:
+            pdb_entries.extend(fe.get_modified_pdb_entries())
+        if emdb_release:
+            emdb_entries.extend(fe.get_emdb_entries())
+        for pdb_entry in pdb_entries:
+            self.entry_list.append((pdb_entry, 'pdb'))
+        for emdb_entry in emdb_entries:
+            self.entry_list.append((emdb_entry, 'emdb'))
 
-    if pdb_release:
-        entries_to_check.setdefault('pdb', []).extend(fe.get_added_pdb_entries())
-    if pdb_modified:
-        entries_to_check.setdefault('pdb', []).extend(fe.get_modified_pdb_entries())
-    if emdb_release:
-        entries_to_check.setdefault('emdb', []).extend(fe.get_emdb_entries())
+    def check_entries(self, output_folder=None):
 
-    for entry_type in entries_to_check:
-        ret = check_entries(entries_to_check[entry_type], entry_type, output_folder=output_folder)
-        missing_files[entry_type] = ret
+        for entry in self.entry_list:
+            entry_id = entry[0]
+            entry_type = entry[1]
+            if entry_type == 'pdb':
+                cr = CheckResult(output_folder=output_folder, pdbid=entry_id)
+            elif entry_type == 'emdb':
+                cr = CheckResult(output_folder=output_folder, emdbid=entry_id)
+            else:
+                logging.error('Unknown entry type')
+                return {}
+            cr.check_entry()
+            missing_ret = cr.get_missing_files()
+            for missing_type in missing_ret:
+                self.return_dictionary.setdefault(entry_type, {}).setdefault(missing_type, []).append(
+                    missing_ret[missing_type])
+                self.failed_entries.append(entry_id)
+            ret_failed = cr.get_failed_programs()
+            if ret_failed:
+                self.entries_with_failed_programs.append(entry_id)
+                for program in ret_failed:
+                    self.return_dictionary.setdefault('failed_programs', {}).setdefault(program, []).append(entry_id)
 
-    pprint(missing_files)
+    def get_full_details(self):
+        return self.return_dictionary
+
+    def get_entries_with_failed_programs(self):
+        return self.entries_with_failed_programs
+
+    def get_failed_entries(self):
+        return self.failed_entries
+
+
+def prepare_entries_and_check(output_folder=None):
+    ce = CheckEntries()
+    ce.get_entries()
+    ce.check_entries(output_folder=output_folder)
+    print('full details of missing entries')
+    pprint(ce.get_full_details())
+    print('entries with missing output: {}'.format(','.join(ce.get_failed_entries())))
+    print('entries with failed programs: {}'.format(','.join(ce.get_entries_with_failed_programs())))
 
 
 def main():
@@ -158,17 +169,16 @@ def main():
     args = parser.parse_args()
     logger.setLevel(args.loglevel)
 
-    prepare_entries_and_check(output_folder=args.output_root, 
-                  entry_file=args.entry_file, 
-                  entry_list=args.entries, 
-                  pdbids=args.pdbids, 
-                  emdbids=args.emdbids,
-                  pdb_release=args.pdb_release,
-                  pdb_modified=args.pdb_modified,
-                  emdb_release=args.emdb_release
-                  )
+    prepare_entries_and_check(output_folder=args.output_root,
+                              entry_file=args.entry_file,
+                              entry_list=args.entries,
+                              pdbids=args.pdbids,
+                              emdbids=args.emdbids,
+                              pdb_release=args.pdb_release,
+                              pdb_modified=args.pdb_modified,
+                              emdb_release=args.emdb_release
+                              )
+
 
 if __name__ == '__main__':
     main()
-
-
