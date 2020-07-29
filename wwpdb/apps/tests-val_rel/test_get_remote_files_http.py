5e34dfcae1a7719e6b7329aa86825d0153a2c2ed
jmb <jmb@ebi.ac.uk>
2020-07-29 23:02:16 +0100

    added options for getting files remotely

--- wwpdb/apps/tests-val_rel/test_get_remote_files_http.py
+++ wwpdb/apps/tests-val_rel/test_get_remote_files_http.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import shutil
 import tempfile
 
 from wwpdb.apps.val_rel.utils.getRemoteFilesHTTP import GetRemoteFiles
@@ -9,7 +10,10 @@ class TestRemoteFilesHTTP(unittest.TestCase):
     def setUp(self):
         self.output_dir = tempfile.mkdtemp()
 
-    def get_emdb_header(self):
+    def tearDown(self):
+        shutil.rmtree(self.output_dir, ignore_errors=True)
+
+    def test_get_emdb_header(self):
         grf = GetRemoteFiles(output_path=self.output_dir)
         grf.get_url(url='http://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-0070/header/emd-0070-v30.xml')
         self.assertTrue(os.path.exists(os.path.join(self.output_dir, 'emd-0070-v30.xml')))
