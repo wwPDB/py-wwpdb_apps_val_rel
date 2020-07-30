49437c753864b047f0d1037b77b9184406d33847
jmb <jmb@ebi.ac.uk>
2020-07-30 21:36:20 +0100

    updated tests

--- wwpdb/apps/tests-val_rel/test_get_files_release_ftp_pdb.py
+++ wwpdb/apps/tests-val_rel/test_get_files_release_ftp_pdb.py
@@ -9,6 +9,9 @@ class TestsGettingEMDBData(unittest.TestCase):
 
     def setUp(self):
         self.temp_folder = tempfile.mkdtemp()
+        self.server = 'ftp.ebi.ac.uk'
+        self.url_prefix = 'pub/databases/pdb/data/structures/all/'
+
 
     def tearDown(self):
         shutil.rmtree(self.temp_folder, ignore_errors=True)
@@ -16,12 +19,16 @@ class TestsGettingEMDBData(unittest.TestCase):
     def test_checking_model_existing_pdb(self):
         gfrf = getFilesReleaseFtpPDB(pdbid='1cbs')
         gfrf.setup_local_temp_ftp(session_path=self.temp_folder)
+        gfrf.server = self.server
+        gfrf.url_prefix = self.url_prefix
         ret = gfrf.get_model()
         self.assertTrue(ret)
 
     def test_checking_header_invalid_pdb(self):
         gfrf = getFilesReleaseFtpPDB(pdbid='1cbssFDSDFSF')
         gfrf.setup_local_temp_ftp(session_path=self.temp_folder)
+        gfrf.server = self.server
+        gfrf.url_prefix = self.url_prefix
         ret = gfrf.get_model()
         self.assertFalse(ret)
 
