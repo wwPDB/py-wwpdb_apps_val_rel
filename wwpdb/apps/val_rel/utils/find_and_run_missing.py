630ca5c3bdc4932f53c0629c74b7c941db08d8a0
jmb <jmb@ebi.ac.uk>
2020-08-02 19:21:31 +0100

    added options to read and write

--- wwpdb/apps/val_rel/utils/find_and_run_missing.py
+++ wwpdb/apps/val_rel/utils/find_and_run_missing.py
@@ -1,23 +1,40 @@
 import argparse
 import logging
+import os
 
 from wwpdb.apps.val_rel.PopulateValidateRelease import PopulateValidateRelease
 from wwpdb.apps.val_rel.utils.check_results import CheckEntries
+from wwpdb.io.locator.ReleasePathInfo import ReleasePathInfo
 
 logger = logging.getLogger(__name__)
 
 
 class FindAndRunMissing:
 
-    def __init__(self):
+    def __init__(self, write_missing=False, read_missing=True):
         self.ce = CheckEntries()
         self.missing_ids = []
         self.missing_file = 'missing.ids'
+        self.rpi = ReleasePathInfo()
 
     def find_missing(self):
         self.ce.get_entries()
         self.ce.check_entries()
         self.missing_ids = self.ce.get_failed_entries()
+
+    def get_missing_file_path(self):
+        return os.path.join(self.rpi.get_for_release_path(), 'missing.ids')
+
+    def write_out_missing(self):
+        if self.missing_ids:
+            with open(self.get_missing_file_path(), 'w') as out_file:
+                for missing_id in self.missing_ids:
+                    out_file.write(missing_id)
+
+    def read_missing_file(self):
+        with open(self.get_missing_file_path()) as in_file:
+            for file_line in in_file:
+                self.missing_ids.append(file_line.strip())
 
     def populate_queue(self):
         if self.missing_ids:
@@ -42,10 +59,14 @@ def main():
         const=logging.DEBUG,
         default=logging.INFO,
     )
+    parser.add_argument('--write_missing', action="store_true")
+    parser.add_argument('--read_missing', action="store_true")
     args = parser.parse_args()
     logger.setLevel(args.loglevel)
 
-    FindAndRunMissing().run_process()
+    frm = FindAndRunMissing(write_missing=args.write_missing,
+                            read_missing=args.read_missing)
+    frm.run_process()
 
 
 if "__main__" in __name__:
