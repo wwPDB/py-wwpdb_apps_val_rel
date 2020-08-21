823e141466102dde470fa0ec33b97c276373ec76
Ezra Peisach <ezra.peisach@rcsb.org>
2020-08-21 12:02:59 -0400

    Add --site-id for find_nd_run_missing. Fix logging setup

--- wwpdb/apps/val_rel/utils/find_and_run_missing.py
+++ wwpdb/apps/val_rel/utils/find_and_run_missing.py
@@ -6,16 +6,18 @@ from wwpdb.apps.val_rel.PopulateValidateRelease import PopulateValidateRelease
 from wwpdb.apps.val_rel.utils.check_results import CheckEntries
 from wwpdb.io.locator.ReleasePathInfo import ReleasePathInfo
 
+# We replace with root if main
 logger = logging.getLogger(__name__)
 
 
 class FindAndRunMissing:
 
-    def __init__(self, write_missing=False, read_missing=True):
-        self.ce = CheckEntries()
+    def __init__(self, write_missing=False, read_missing=True, siteID=None):
+        self.__siteid = siteID
+        self.ce = CheckEntries(siteID=self.__siteid)
         self.missing_ids = []
         self.missing_file = 'missing.ids'
-        self.rpi = ReleasePathInfo()
+        self.rpi = ReleasePathInfo(siteId=self.__siteid)
         self.write_missing = write_missing
         self.read_missing = read_missing
 
@@ -31,7 +33,7 @@ class FindAndRunMissing:
         if self.missing_ids:
             with open(self.get_missing_file_path(), 'w') as out_file:
                 for missing_id in self.missing_ids:
-                    out_file.write(missing_id)
+                    out_file.write("%s\n" % missing_id)
 
     def read_missing_file(self):
         with open(self.get_missing_file_path()) as in_file:
@@ -42,6 +44,7 @@ class FindAndRunMissing:
         if self.missing_ids:
             pvr = PopulateValidateRelease(entry_list=self.missing_ids,
                                           validation_sub_dir='missing',
+                                          site_id=self.__siteid
                                           )
             pvr.run_process()
 
@@ -57,6 +60,11 @@ class FindAndRunMissing:
 
 
 def main():
+    # Root logger
+    logger = logging.getLogger()
+    log_format = "%(funcName)s (%(levelname)s) - %(message)s"
+    logging.basicConfig(format=log_format)
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-d",
@@ -69,11 +77,14 @@ def main():
     )
     parser.add_argument('--write_missing', action="store_true")
     parser.add_argument('--read_missing', action="store_true")
+    parser.add_argument("--site_id", help="site id to get files from", type=str)
+
     args = parser.parse_args()
     logger.setLevel(args.loglevel)
 
     frm = FindAndRunMissing(write_missing=args.write_missing,
-                            read_missing=args.read_missing)
+                            read_missing=args.read_missing,
+                            siteID=args.site_id)
     frm.run_process()
 
 
