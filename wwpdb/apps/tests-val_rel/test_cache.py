717845a9ec707574f80bd340a9c26ae55a2f995b
Ezra Peisach <ezra.peisach@rcsb.org>
2020-11-19 17:35:21 -0500

    File cache and test - not integrated

---
+++ wwpdb/apps/tests-val_rel/test_cache.py
@@ -0,0 +1,66 @@
+
+import unittest
+import os
+
+from wwpdb.apps.val_rel.utils.PersistFileCache import PersistFileCache
+
+
+class PersisFileCacheTests(unittest.TestCase):
+    def setUp(self):
+        self.__cache = "/tmp/cache"
+
+    def testFileAccess(self):
+        """Test adding and access to file"""
+
+        pfc = PersistFileCache(self.__cache)
+        lfile = __file__
+        cfname = "/tmp/somewhere/file.txt"
+
+        ret = pfc.add_file(lfile, cfname)
+        self.assertTrue(ret, "Adding file failed")
+
+        self.assertTrue(pfc.exists(cfname))
+
+        outfile = "/tmp/testout.txt"
+        self.assertTrue(pfc.exists(cfname))
+
+        self.assertTrue(pfc.get_file(cfname, outfile))
+
+        self.assertTrue(os.path.exists(outfile))
+
+        self.assertTrue(abs(os.path.getmtime(outfile) - os.path.getmtime(lfile)) < 1)
+
+    def testNegativeCache(self):
+        """Test adding to negative cache"""
+
+        pfc = PersistFileCache(self.__cache)
+        cfname = "/tmp/somewhereelse/file.txt-negative"
+
+        self.assertTrue(pfc.add_negative_cache(cfname))
+
+        self.assertTrue(pfc.is_negative_cache(cfname))
+
+        # Adding again should be ok
+        self.assertTrue(pfc.add_negative_cache(cfname))
+
+    def testCacheFileStatus(self):
+        """Test testing fail status"""
+
+        pfc = PersistFileCache(self.__cache)
+        lfile = __file__
+        cfname = "/tmp/somewherenew/file.txt"
+        cfname2 = "/tmp/somewhereelse/file.txt"
+        cfname3 = "/tmp/somewhereelse/file.txt.new"
+
+        ret = pfc.add_file(lfile, cfname)
+        self.assertTrue(ret, "Adding file failed")
+
+        self.assertTrue(pfc.add_negative_cache(cfname2))
+
+        self.assertEqual(pfc.cache_file_status(cfname), True)
+        self.assertEqual(pfc.cache_file_status(cfname2), False)
+        self.assertEqual(pfc.cache_file_status(cfname3), None)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
---
+++ wwpdb/apps/val_rel/utils/PersistFileCache.py
@@ -0,0 +1,171 @@
+##
+# File:  PersistsFileCache.py
+# Date:  19-Nov-2020Sep-2012
+#
+# Updates:
+#
+##
+"""
+ Implements a local file object cache for basic operations
+
+ Will also allow negative caching of files using a per directory admin file
+"""
+import os
+import json
+import logging
+
+from oslo_concurrency import lockutils
+
+from wwpdb.io.file.DataFile import DataFile
+
+logger = logging.getLogger(__name__)
+
+
+class PersistFileCache(object):
+    def __init__(self, cache_dir="/tmp"):
+        self.__cache_dir = cache_dir
+        lockutils.set_defaults(self.__cache_dir)
+
+    def __getcachedir(self, fpath):
+        """Returns the internal cachedir for fpath"""
+        if fpath is None:
+            return None
+
+        # Remove starting slash if present on filename
+        if fpath[0] == "/":
+            fpath = fpath[1:]
+
+        if len(fpath) == 0:
+            return None
+
+        basedir = os.path.dirname(fpath)
+        return os.path.join(self.__cache_dir, basedir)
+
+    def __getcachefile(self, fpath):
+        """Returns filepath of fpath in cache"""
+        cd = self.__getcachedir(fpath)
+        if cd is None:
+            return None
+
+        bname = os.path.basename(fpath)
+
+        cache_path = os.path.join(cd, bname)
+        return cache_path
+
+    @lockutils.synchronized("sessiondatastore.lock", external=True)
+    def add_file(self, realfpath, fpath):
+        """Adds the contents of realfpath to the cache as fpath.
+           Overwrites if present.
+
+           Returns True on success
+        """
+
+        cache_dir = self.__getcachedir(fpath)
+        if not cache_dir:
+            return False
+
+        cache_file = self.__getcachefile(fpath)
+
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+
+        df = DataFile(realfpath)
+        df.copy(dstPath=cache_file)
+
+        return True
+
+    @lockutils.synchronized("sessiondatastore.lock", external=True)
+    def get_file(self, fpath, realfpath):
+        """Retrieves fpath from the cache and copies it to realfpath
+           If fpath is not in cache, returns False, else True.
+
+           Timestamp will be preserved
+        """
+
+        cache_file = self.__getcachefile(fpath)
+        if cache_file is None:
+            return False
+
+        if not os.path.exists(cache_file):
+            return False
+
+        df = DataFile(cache_file)
+        df.copy(dstPath=realfpath)
+        return True
+
+    @lockutils.synchronized("sessiondatastore.lock", external=True)
+    def exists(self, fpath):
+        """Returns True if fpath in cache, else False"""
+        cache_file = self.__getcachefile(fpath)
+        return os.path.exists(cache_file)
+
+    def __getmissingfilepath(self, fpath):
+        """Returns the file path for the missing file"""
+        cache_dir = self.__getcachedir(fpath)
+        return os.path.join(cache_dir, "missing.json")
+
+    def __getmissinglist(self, fpath):
+        """Retrieves the json data for missing files"""
+        mpath = self.__getmissingfilepath(fpath)
+
+        if os.path.exists(mpath):
+            with open(mpath, "r") as fin:
+                jdata = json.load(fin)
+                return jdata
+        return []
+
+    def __writemissinglist(self, fpath, mlist):
+        """Writes the json data for missing files"""
+        mpath = self.__getmissingfilepath(fpath)
+
+        with open(mpath, "w") as fout:
+            json.dump(mlist, fout)
+        return True
+
+    @lockutils.synchronized("sessiondatastore.lock", external=True)
+    def add_negative_cache(self, fpath):
+        cache_dir = self.__getcachedir(fpath)
+
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+
+        mlist = self.__getmissinglist(fpath)
+        if fpath not in mlist:
+            mlist.append(fpath)
+            ret = self.__writemissinglist(fpath, mlist)
+        else:
+            ret = True
+
+        return ret
+
+    @lockutils.synchronized("sessiondatastore.lock", external=True)
+    def is_negative_cache(self, fpath):
+        mlist = self.__getmissinglist(fpath)
+        if fpath in mlist:
+            return True
+        return False
+
+    @lockutils.synchronized("sessiondatastore.lock", external=True)
+    def cache_file_status(self, fpath):
+        """Returns one of three statuses for fpath
+        True if have a file
+        False if negative cache
+        None - cache does not know about file.
+        """
+
+        cache_file = self.__getcachefile(fpath)
+        # Cannot make a name of the file
+        if cache_file is None:
+            return None
+
+        # If file in cache
+        if os.path.exists(cache_file):
+            return True
+
+        # Check negative cache
+        mlist = self.__getmissinglist(fpath)
+        if fpath in mlist:
+            return False
+
+        # We known nothing
+        return None
