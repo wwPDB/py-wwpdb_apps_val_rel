469d1f45129eea7c4ecf8abc4223075c57e1c7b6
Ezra Peisach <ezra.peisach@rcsb.org>
2020-11-22 07:53:30 -0500

    Require oslo_concurrency as a dependency

--- setup.py
+++ setup.py
@@ -44,8 +44,8 @@ setup(
     #
     install_requires=['wwpdb.utils.config', 'wwpdb.utils.detach',
                       'wwpdb.utils.dp', 'wwpdb.utils.message_queue',
-                      'wwpdb.apps.validation', 'wwpdb.io~=0.10.dev2',
-                      'wwpdb.utils.ws_utils'
+                      'wwpdb.apps.validation', 'wwpdb.io~=0.10',
+                      'wwpdb.utils.ws_utils', 'oslo_concurrency'
                       ],
     packages=find_packages(exclude=['wwpdb.apps.tests-val_rel',
                                     'mock-data']),
